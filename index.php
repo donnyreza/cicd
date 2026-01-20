42a1d7d99f5d988ae99ecea0f51f9d02e89f64c7
gitunikom <82491094+gitunikom@users.noreply.github.com>
2026-01-20 18:02:50 +0700

    Fixed Error

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "Latihan CI/CD Menggunakan PHP, Github & Railway";
-eho "<br>Tambahkan Konten File";
+echo "<br>Tambahkan Konten File. Setelah diperbaiki";
 echo "<br>Update lagi konten file lagi setelah gagal";
